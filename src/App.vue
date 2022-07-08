57d0b5d4430f75d6f271f3226d614997c8aed958
westhide <mr.westhide@gmail.com>
2022-07-08 20:25:59 +0800

    init: first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div>Vue Sheet</div>
+</template>
+
+<style>
+#app {
+}
+</style>
