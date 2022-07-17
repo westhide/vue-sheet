22a5f10d40452e79e1f9b506357b5dc8f9fa57b1
westhide <1084465086@qq.com>
2022-07-17 17:52:26 +0800

    ci(report): rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ import Inspect from "vite-plugin-inspect";
 import Compression from "vite-plugin-compression";
 import { chunkSplitPlugin as ChunkSplit } from "vite-plugin-chunk-split";
 import Imagemin from "vite-plugin-imagemin";
-import Legacy from "@vitejs/plugin-legacy";
+// import Legacy from "@vitejs/plugin-legacy";
+import { visualizer as Visualizer } from "rollup-plugin-visualizer";
 
 import Pkg from "./package.json";
 import Banner from "vite-plugin-banner";
@@ -87,7 +88,7 @@ export default defineConfig({
           "@vueuse/integrations/useChangeCase": ["useChangeCase"],
         },
         {
-          lodash: [
+          "lodash-es": [
             "cloneDeep",
             "defaultsDeep",
             "isEmpty",
@@ -142,11 +143,18 @@ export default defineConfig({
     }),
 
     // TODO: [vite:build-html] Cannot destructure property 'renderBuiltUrl' of 'config.experimental' as it is undefined
-    Legacy(),
+    // Legacy(),
 
     Banner(
       `/**\n * name: ${Pkg.name}\n * version: v${Pkg.version}\n * description: ${Pkg.description}\n * author: ${Pkg.author}\n * homepage: ${Pkg.homepage}\n */`
     ),
+
+    Visualizer({
+      filename: "./node_modules/.cache/visualizer/stats.html",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
   // experimental: {},
   test: {
